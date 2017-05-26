a9bedd79fc6d51fa60b24f5a710a390d89a7c046
Erik Peldan <erik.peldan@gmail.com>
2017-05-26 20:33:49 +0200

    Parse options

--- engine.lisp
+++ engine.lisp
@@ -115,3 +115,91 @@ option name Slow Mover type spin default 100 min 10 max 1000
 option name UCI_Chess960 type check default false
 option name UCI_AnalyseMode type check default false
 ")
+
+
+(defclass option ()
+  ((name :type string
+         :documentation "Option name"
+         :accessor option-name
+         :initarg :name)))
+
+
+(defclass button (option)
+  ())
+
+(defclass check (option)
+  ((value :documentation "Option value"
+          :accessor option-value
+          :initarg :value)))
+
+(defclass string-option (option)
+  ((value :documentation "Option value"
+          :accessor option-value
+          :initarg :value)))
+
+(defclass spin (option)
+  ((value :documentation "Option value"
+          :accessor option-value
+          :initarg :value)
+   (min :documentation "Min Value"
+        :accessor option-min-value
+        :initarg :min)
+   (max :documentation "Max Value"
+        :accessor option-max-value
+        :initarg :max)))
+
+
+(defun parse-min (string)
+  (let ((min (+ 1 (length "min") (search "min" string))))
+    (parse-integer
+     (subseq string
+             min
+             (search " " string :start2 min)))))
+
+
+(defun parse-max (string)
+  (let ((max (+ 1 (length "max") (search "max" string))))
+    (parse-integer
+     (subseq string
+             max
+             (search " " string :start2 max)))))
+
+(defun parse-option-specific (type name value rest)
+  (ecase type
+    (:spin (make-instance 'spin :name name :value (parse-integer value) :min (parse-min rest) :max (parse-max rest)))
+    (:string (make-instance 'string-option :name name :value value))
+    (:check (make-instance 'check :name name :value value))
+    (:button (make-instance 'check :name name))))
+
+(defun parse-option (string)
+  (let* ((name (search "name" string))
+         (type (search "type" string :start2 name))
+         (default (search "default" string :start2 type))
+         name-end type-end default-end rest)
+
+    (unless (and name type default)
+      (error "Invalid option '~a'" string))
+
+    ;;
+    ;; Move to the end of each respective string
+    ;;
+    (setf name-end (+ name (length "name") 1))
+    (setf type-end (+ type (length "type") 1))
+    (setf default-end (+ default (length "default") 1))
+    (setf rest (search " " string :start2 default-end))
+
+    (let ((name-str (subseq string name-end (- type 1)))
+          (type-str (subseq string type-end (- default 1)))
+          (default-str (if rest
+                           (subseq string default-end (- rest 1))
+                           (subseq string default-end)))
+          (remainder (if rest (subseq string rest) nil)))
+
+      (parse-option-specific (find type-str '(:check :spin :button :string)
+                                   :test #'string-equal)
+                             name-str default-str remainder))))
+
+
+;;(parse-option "option name UCI_AnalyseMode type check default false")
+;;(parse-option "option name Book File type string default book.bin")
+;;(parse-option "option name Slow Mover type spin default 100 min 10 max 1000")
