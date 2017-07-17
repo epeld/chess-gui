9bd87416921d8abb8db4c6679dfc2ce207f50085
Erik Peldan <erik.peldan@gmail.com>
2017-07-17 18:04:28 +0200

    Load all available piece textures

--- chessboard.lisp
+++ chessboard.lisp
@@ -23,6 +23,13 @@
 (defconstant +num-cols+ 8)
 (defconstant +num-rows+ 8)
 
+(defun square-color (row col)
+  "Sets the gl square color, given a row and col"
+  (if (zerop (mod (+ row col) 2))
+      (apply #'gl:color *square-light-color*)
+      (apply #'gl:color *square-dark-color*)))
+
+
 (defun chess-board ()
   "Render a chessboard"
 
@@ -43,18 +50,37 @@
       
       (loop for i upto +num-rows+ do
            (loop for j upto +num-cols+ do
-                (if (zerop (mod (+ i j) 2))
-                    (apply #'gl:color *square-light-color*)
-                    (apply #'gl:color *square-dark-color*))
-                (render:quad i j w h))))))
+                (square-color i j)
+                (render:quad i j w h)))
+
+
+      ;;(square-color 3 4)
+      (render:quad 3 3 w h "BlackQueen")
+      )))
+
+
+(defun load-piece-textures ()
+  "Load all known piece textures"
+
+  (let ((pieces '(:pawn :bishop :knight :rook :queen :king))
+        (colors '(:white :black)))
+    
+    (loop for piece in pieces do
+         (loop for color in colors do
+              (let ((path (format nil "./bitmaps/~:(~a~)~:(~a~)_64.png" color piece))
+                    (name (format nil "~:(~a~)~:(~a~)" color piece)))
+                (texture:get-texture-from-png-file path name))))))
 
 
 (defmethod application:application-render ((board chess-board-application))
+  (load-piece-textures)
+  
   (gl:clear-color 0.5 0.05 0.05 1.0) ;; Nice snake color: (gl:clear-color 0 0.5 0.25 1.0)
+  (gl:color 0 1 0)
   (gl:clear :color-buffer)
   (chess-board)
 
   (glut:swap-buffers))
 
 
-(application:run-application (new-instance))
+(application:run-application (new-instance)) 
